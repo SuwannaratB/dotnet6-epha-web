ec19c6fd5cd3014ce699d9bc9435708e9c75891e
kanompang962 <kanompang962@gmail.com>
2024-09-11 14:11:20 +0700

    check auth

--- wwwroot/js/_flow/jsea.js
+++ wwwroot/js/_flow/jsea.js
@@ -1462,14 +1462,7 @@ AppMenuPage.controller("ctrlAppPage", function ($scope, $http, $filter, conFig, 
     
 
     function page_load() {
-
         arr_def();
-
-        if ($scope.user_name == null) {
-            window.open('login/index', "_top");
-            return;
-        }
-
         get_data(true, false);
     }
 
